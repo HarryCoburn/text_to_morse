8c732d76a685ec0fd85514be062ca37cc96320c5
HarryCoburn <10048077+HarryCoburn@users.noreply.github.com>
2023-06-28 17:18:19 -0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,49 @@
+# Text to Morse Code
+# Harry Coburn
+
+MORSE_KEY = {
+    "a": ".-",
+    "b": "-...",
+    "c": "-.-.",
+    "d": "-..",
+    "e": ".",
+    "f": "..-.",
+    "g": "--.",
+    "h": "....",
+    "i": "..",
+    "j": ".---",
+    "k": "-.-",
+    "l": ".-..",
+    "m": "--",
+    "n": "-.",
+    "o": "---",
+    "p": ".--.",
+    "q": "--.-",
+    "r": ".-.",
+    "s": "...",
+    "t": "-",
+    "u": "..-",
+    "v": "..._",
+    "w": ".--",
+    "x": "-..-",
+    "y": "-.--",
+    "z": "--..",
+    "1": ".----",
+    "2": "..---",
+    "3": "...--",
+    "4": "....-",
+    "5": ".....",
+    "6": "-....",
+    "7": "--...",
+    "8": "---..",
+    "9": "----.",
+    "0": "-----",
+    " ": " ",
+}
+
+text = input("Input message to turn into Morse: ").lower()
+
+print(" ".join('' if c not in MORSE_KEY else MORSE_KEY[c] for c in text))
+
+# At first, I did a simple for loop into a list and joined it. Then I thought about a map, but a little
+# searching found this way of using an if/then/else inside of the join method.
